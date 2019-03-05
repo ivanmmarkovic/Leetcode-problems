8e0d519dc43479379f78692714c9d0babd73427c
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-03-04 16:57:54 -0800

    Update Main.java

--- String/stringCompression/Main.java
+++ String/stringCompression/Main.java
@@ -1,57 +1,50 @@
-package stringCompression;
-
 /*
  * Given a string, write a function to compress it by 
  * shortening every sequence of the same character to that 
- * character followerd by the number of repetitions. 
+ * character followed by the number of repetitions. 
  * If the compressed string is longer than the original, 
  * you should return original string 
  * */
-
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("-->" + compression("a"));
-		System.out.println("-->" + compression("ab"));
-		System.out.println("-->" + compression("aab"));
-		System.out.println("-->" + compression("aaabbb"));
-		System.out.println("-->" + compression("aaabccddeee"));
+		System.out.println("-->" + compress("a"));
+		System.out.println("-->" + compress("ab"));
+		System.out.println("-->" + compress("aab"));
+		System.out.println("-->" + compress("aaabbb"));
+		System.out.println("-->" + compress("aaabccddeee"));
 	}
 	
-	public static String compression(String string) {
+	public static String compress(String string) {
 		if(string.length() == 1)
 			return string;
 		else {
-			ArrayList<String> compressed = new ArrayList<>();
-			int counter = 1, siblingsLength = 1;
+			String result = "";
+			int connectedCount = 1, count = 1;
 			char currentCharacter = string.charAt(0);
-			while(counter < string.length()) {
-				if(currentCharacter == string.charAt(counter)) {
-					siblingsLength++;
+			while(count < string.length()) {
+				if(string.charAt(count) == currentCharacter) {
+					connectedCount++;
 				}
 				else {
-					compressed.add(String.valueOf(currentCharacter));
-					if(siblingsLength > 1)
-						compressed.add(String.valueOf(siblingsLength));
-					currentCharacter = string.charAt(counter);
-					siblingsLength = 1;
+					result += currentCharacter;
+					if(connectedCount > 1) 
+						result += connectedCount;
+					currentCharacter = string.charAt(count);
+					connectedCount = 1;
 				}
-				// on exit
-				if(counter == string.length() - 1) {
-					compressed.add(String.valueOf(currentCharacter));
-					if(siblingsLength > 1)
-						compressed.add(String.valueOf(siblingsLength));
+				
+				if(count == string.length() - 1) { // on exit
+					result += string.charAt(count);
+					if(connectedCount > 1) 
+						result += connectedCount;
 				}
-				counter++;
+				count++;
 			}
-			String result = "";
-			for(String s: compressed)
-				result += s;
 			return result;
 		}
+		
 	}
 
 }
