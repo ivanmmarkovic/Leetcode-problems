9346d28704088bbcbbc7478ccdf9ed2e863d0a2c
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-07-20 16:09:34 -0700

    Add files via upload

---
+++ Trees/lowest-common-ancestor-of-a-binary-tree-iterative/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+    }
+    
+    public static BinaryTree commonAncestor(BinaryTree bt1, BinaryTree bt2) {
+        BinaryTree tmp = bt2;
+        boolean found = false;
+        while(bt1 != null && !found) {
+            while(tmp != null && !found) {
+                if(bt1 == tmp)
+                    found = true;
+                else
+                    tmp = tmp.parent;
+            }
+            if(!found) {
+                bt1 = bt1.parent;
+                tmp = bt2;
+            }
+        }
+        return bt1; 
+    }
+
+}
