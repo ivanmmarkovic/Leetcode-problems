92321089e7ff4e31498c036bf87960e11d3cc819
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-10-14 07:45:13 -0700

    Update Main.java

--- Trees/is-binary-tree-balanced/Main.java
+++ Trees/is-binary-tree-balanced/Main.java
@@ -8,24 +8,24 @@ public class Main {
 		
 	}
 	
-	public static int getHeight(BinaryTree bt) {
-		int left = 0, right = 0;
-		if(bt.leftChild != null)
-			left += getHeight(bt.leftChild);
-		if(bt.rightChild != null)
-			right += getHeight(bt.rightChild);
-		return Math.max(left, right) + 1;
-	}
 	public static boolean isBalanced(BinaryTree bt) {
 		if(bt == null)
 			return true;
-		int left = bt.leftChild != null ? getHeight(bt.leftChild) : 0;
-		int right = bt.rightChild != null ? getHeight(bt.rightChild) : 0;
-		if(Math.abs(left - right) <= 1) {
+		int left = getTreeHeight(bt.leftChild);
+		int right = getTreeHeight(bt.rightChild);
+		if(Math.abs(left - right) <= 1)
 			return isBalanced(bt.leftChild) && isBalanced(bt.rightChild);
-		}
-		else {
+		else
 			return false;
-		}
+	}
+	
+	public static int getTreeHeight(BinaryTree bt) {
+		if(bt == null)
+			return 0;
+		
+		int leftHeight = getTreeHeight(bt.leftChild);
+		int rightHeight = getTreeHeight(bt.rightChild);
+		
+		return Math.max(leftHeight, rightHeight) + 1;
 	}
 }
