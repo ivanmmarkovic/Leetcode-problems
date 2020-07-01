8162a9d98b5e422934cf18c2c9aacbd0045f0bfc
Ivan Markovic <ivanmdmarkovic@gmail.com>
2020-07-01 10:28:35 -0700

    Update MaxAreaOfIsland.java

--- graphs/MaxAreaOfIsland.java
+++ graphs/MaxAreaOfIsland.java
@@ -1,11 +1,15 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 695. Max Area of Island
 Medium
 
 https://leetcode.com/problems/max-area-of-island/
 
-Runtime: 25 ms, faster than 5.47% of Java online submissions for Max Area of Island.
-Memory Usage: 40.6 MB, less than 96.30% of Java online submissions for Max Area of Island.
+Runtime: 4 ms, faster than 30.59% of Java online submissions for Max Area of Island.
+Memory Usage: 39.4 MB, less than 97.18% of Java online submissions for Max Area of Island.
 
 Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
 
@@ -38,60 +42,53 @@ Submissions
 
 */
 class MaxAreaOfIsland {
-    public int maxAreaOfIsland(int[][] grid) {
-        Set<String> visited = new HashSet<>();
-        int result = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                if(grid[i][j] == 1 && !containsCoords(visited, i, j)){
-                    Integer coords[] = {i, j};
-                    addCoords(visited, i, j);
-                    Queue<Integer[]> queue = new LinkedList<>();
-                    queue.add(coords);
-                    int total = 0;
-                    while(!queue.isEmpty()){
-                        Integer tmp[] = queue.poll();
-                        total += 1;
-                        int x = tmp[0];
-                        int y = tmp[1];
-                        lookForNeighbours(grid, x, y, queue, visited);                     
-                    }
-                    if(total > result)
-                        result = total;
-                }
-            }
-        }
-        return result;
+	public int maxAreaOfIsland(int[][] grid) {
+        boolean visited[][] = new boolean[grid.length][];
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = new boolean[grid[i].length];
+		}
+		int result = 0;
+		for(int i = 0; i < grid.length; i++) {
+			for(int j = 0; j < grid[i].length; j++) {
+				if(grid[i][j] == 1 && !visited[i][j]) {
+					Queue<int[]> queue  = new LinkedList<int[]>();
+					int coords[] = {i, j};
+					queue.add(coords);
+					visited[i][j] = true;
+					int tmpCount = 0;
+					while (!queue.isEmpty()) {
+						coords = queue.poll();
+						tmpCount += 1;
+						addNeighbours(coords, grid, visited, queue);
+					}
+					if(tmpCount > result)
+						result = tmpCount;
+				}
+			}
+		}
+		return result;
     }
     
-    private void addCoords(Set<String> visited, int x, int y){
-        visited.add("x:" + x +"y:" + y);
-    }
-    
-    private boolean containsCoords(Set<String> visited, int x, int y){
-        return visited.contains("x:" + x +"y:" + y);
-    }
-    
-    private void lookForNeighbours(int grid[][], int x, int y, Queue<Integer[]> queue, Set<String> visited){
-        if(y < grid[x].length - 1 && grid[x][y + 1] == 1 && !containsCoords(visited, x, y + 1)){
-            addCoords(visited, x, y + 1);
-            Integer neighbour[] = {x, y + 1};
-            queue.add(neighbour);
-        }
-        if(y > 0 && grid[x][y - 1] == 1 && !containsCoords(visited, x, y - 1)){
-            addCoords(visited, x, y - 1);
-            Integer neighbour[] = {x, y - 1};
-            queue.add(neighbour);
-        }
-        if(x < grid.length - 1 && grid[x + 1][y] == 1 && !containsCoords(visited, x + 1, y)){
-            addCoords(visited, x + 1, y);
-            Integer neighbour[] = {x + 1, y};
-            queue.add(neighbour);
-        }
-        if(x > 0 && grid[x - 1][y] == 1 && !containsCoords(visited, x - 1, y)){
-            addCoords(visited, x - 1, y);
-            Integer neighbour[] = {x - 1, y};
-            queue.add(neighbour);
-        } 
-    }
+    private void addNeighbours(int[] coords, int[][] grid, boolean[][] visited, Queue<int[]> queue) {
+		int x = coords[0];
+		int y = coords[1];
+		if(y < grid[x].length - 1 &&  grid[x][y + 1] == 1 && !visited[x][y + 1]) {
+			addToQueue(x, y + 1, visited, queue);
+		}
+		if(y > 0 && grid[x][y - 1] == 1 && !visited[x][y - 1]) {
+			addToQueue(x, y - 1, visited, queue);
+		}
+		if(x < grid.length - 1 && grid[x + 1][y] == 1 && !visited[x + 1][y]) {
+			addToQueue(x + 1, y, visited, queue);
+		}
+		if(x > 0 && grid[x - 1][y] == 1 && !visited[x - 1][y]) {
+			addToQueue(x - 1, y, visited, queue);
+		}
+	}
+
+	private void addToQueue(int x, int y, boolean[][] visited, Queue<int[]> queue) {
+		visited[x][y] = true;
+		int xy[] = {x, y};
+		queue.add(xy);
+	}
 }
