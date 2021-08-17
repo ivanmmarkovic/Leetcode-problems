e58d64d5976b355d08013dae4e1aa8b6269577d8
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-08-17 23:36:17 +0200

    Count Good Nodes in Binary Tree

---
+++ trees/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,94 @@
+package trees;
+
+/*
+
+1448. Count Good Nodes in Binary Tree
+Medium
+
+https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+Runtime: 2 ms, faster than 96.94% of Java online submissions for Count Good Nodes in Binary Tree.
+Memory Usage: 48.2 MB, less than 26.89% of Java online submissions for Count Good Nodes in Binary Tree.
+
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+ 
+
+Constraints:
+
+    The number of nodes in the binary tree is in the range [1, 10^5].
+    Each node's value is between [-10^4, 10^4].
+
+Accepted
+97,556
+Submissions
+133,806
+*/
+public class CountGoodNodesInBinaryTree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public int goodNodes(TreeNode root) {
+		return goodNodesHelper(root, null);
+	}
+
+	private int goodNodesHelper(TreeNode root, Integer max) {
+		if (root == null)
+			return 0;
+		int count = 0;
+		if (max == null) {
+			count++;
+			max = root.val;
+		} else if (max <= root.val) {
+			count++;
+		}
+
+		count += goodNodesHelper(root.left, Math.max(max, root.val));
+		count += goodNodesHelper(root.right, Math.max(max, root.val));
+		return count;
+	}
+
+}
