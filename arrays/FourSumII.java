73a8bda2446673caee6a66e7555138fcba6569c9
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-02-07 20:05:51 +0100

    4 sum ii

---
+++ arrays/FourSumII.java
@@ -0,0 +1,73 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+454. 4Sum II
+Medium
+
+https://leetcode.com/problems/4sum-ii/
+
+Runtime: 160 ms, faster than 69.67% of Java online submissions for 4Sum II.
+Memory Usage: 57.3 MB, less than 50.00% of Java online submissions for 4Sum II.
+
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+    0 <= i, j, k, l < n
+    nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+Output: 2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+
+Example 2:
+
+Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+Output: 1
+
+ 
+
+Constraints:
+
+    n == nums1.length
+    n == nums2.length
+    n == nums3.length
+    n == nums4.length
+    1 <= n <= 200
+    -228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+
+Accepted
+240,143
+Submissions
+422,450
+
+*/
+public class FourSumII {
+
+	public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int n = nums1.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int sum = nums1[i] + nums2[j];
+                map.put(sum, map.getOrDefault(sum, 0)+1);
+            }
+        }
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int sum = nums3[i] + nums4[j];
+                result += map.getOrDefault(-sum, 0);
+            }
+        }
+        return result;
+    }
+}
