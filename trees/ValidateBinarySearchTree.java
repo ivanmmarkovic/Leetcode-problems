b2b2826bc02d332605d87550d0002d51a99f70ca
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-03-23 19:24:50 +0000

    Validate binary search tree

--- trees/ValidateBinarySearchTree.java
+++ trees/ValidateBinarySearchTree.java
@@ -24,4 +24,14 @@ public class ValidateBinarySearchTree {
 			return false;
 		return true;
 	}
+
+	private boolean isValidII(TreeNode root, Integer min, Integer max) {
+		if(root == null)
+			return true;
+		if(min != null && root.val <= min)
+			return false;
+		if(max != null && root.val >= max)
+			return false;
+		return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+	}
 }
