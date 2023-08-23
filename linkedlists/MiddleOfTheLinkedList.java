3b5f64ee0deeca64d88f4d1ff0717f0ab4752a8c
Ivan Markovic <ivanmdmarkovic@gmail.com>
2023-08-23 13:41:35 +0200

    Update MiddleOfTheLinkedList.java

--- linkedlists/MiddleOfTheLinkedList.java
+++ linkedlists/MiddleOfTheLinkedList.java
@@ -57,6 +57,23 @@ class MiddleOfTheLinkedList {
     }
 
     public ListNode middleNode(ListNode head) {
+	    
+        ListNode slow = head, fast = head;
+
+        while(fast != null){
+            if(fast.next == null)
+                return slow;
+            if(fast.next.next == null)
+                return slow.next;
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return null;
+    }
+    /*
+    public ListNode middleNode(ListNode head) {
         
         ListNode slow = head, fast = head, result = null;
         boolean stopped = false;
@@ -76,4 +93,5 @@ class MiddleOfTheLinkedList {
         }
         return result;   
     }
+    */
 }
